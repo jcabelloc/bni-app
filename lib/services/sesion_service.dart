d086f69996e4c70c4b4a281cb36690c4502e2d2e
Miguel Aspilcueta <maspilcueta@itana.pe>
2020-06-15 18:32:23 -0500

    BNI-32 En sesion service se realizo una validacion al momento de obtener la proxima sesion

--- lib/services/sesion_service.dart
+++ lib/services/sesion_service.dart
@@ -5,16 +5,17 @@ class SesionService {
   final _firestore = Firestore.instance;
 
   Future<Sesion> getProximaSesion(String idGrupo) async {
-    DocumentSnapshot doc = (await _firestore
+    DocumentSnapshot doc;
+    final documentos = (await _firestore
             .collection(Sesion.collectionId)
             .where('idGrupo', isEqualTo: idGrupo)
             .where('fechaHora',
                 isGreaterThan: Timestamp.fromDate(DateTime.now()))
             .orderBy('fechaHora')
             .limit(1)
-            .getDocuments())
-        .documents
-        .first;
+            .getDocuments());
+
+    if (documentos.documentChanges.isNotEmpty) doc = documentos.documents.first;
 
     return doc != null ? Sesion.fromSnapshot(doc) : null;
   }
